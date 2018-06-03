e548547c52ca5c038941c9b4e5c86a091449e790
masterlink64 <steve.cao7@gmail.com>
2018-06-02 18:39:14 -0700

    added feature to clear form after submission, clarify why this works

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-
-// using an event listener on click, toggle class between far and fas
-// come back to this event listener for the bonus
-$(".fa-star").on("click", function() {
-    $(this).toggleClass("far fas");
+// wrap document ready function around my whole JS/jquery to wait for DOM to load
+// just in case and good practice? can also short had as $(function() {});
+$(document).ready(() => {
+    // using an event listener on click, toggle class between far and fas
+    // come back to this event listener for the bonus
+    // Is this recommended? creating a function for this purpose to make smaller digestable code?
+    function favoriteStar() {
+        $(".fa-star").on("click", function() {
+            $(this).toggleClass("far fas");
+        });
+    }; // can I write IIFE here?
+    // function that will prevent default when you click the submit btn in the form
+    // and instead add a li element to the ordered list
+    // will need to grab values from form
+    function addStory(event) {
+        // click is a jQuery for another way of saying on click or evenlistener for click
+        $(".btn.submit-story").click(function(event) {
+            event.preventDefault();
+            console.log($("#story").val());
+            console.log($("#data").val());
+            // clarify why this works
+            $('#submit-form').each(function(){
+                this.reset();
+            });
+        })
+        // reset form to blank
+    };
+    addStory();
+    favoriteStar();
 });

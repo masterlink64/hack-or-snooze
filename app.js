272b1a10c391154adbabe25426d067483bdbdb9a
masterlink64 <steve.cao7@gmail.com>
2018-06-03 16:56:48 -0700

    finished the favorite featuregit add .! yay

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ $(document).ready(() => {
             $(this).toggleClass("far fas");  
             // need to append to favorite list when it is clicked
               // how to get the information we want?
-            favList.append(`<li class="story"><i class="far fa-star"></i> <a href="${dataVal}">${storyVal}</a></li>`)
+            //favList.append(`<li class="story"><i class="far fa-star"></i> <a href="${dataVal}">${storyVal}</a></li>`)
         });
     }; // can I write IIFE here?
     // function that will prevent default when you click the submit btn in the form
@@ -41,16 +41,54 @@ $(document).ready(() => {
     };
     // write a function to hide current list and show favorite list
     function showFavorite() {
-        let favorite = $('.favoriteNav');
+        let favoriteNav = $('.favoriteNav');
+        let navItems = $('#navbarNavAltMarkup');
         let allStories = $('.all-stories');
+        let favStories = $('.fav-list');
         // when you click the favorite text on the nav bar do certain steps
           // hide current content
           // display new ordered list
-        favorite.click(function(event) {
-            allStories.hide();
+        // event delegation to add event listener to the favortie nav button
+        navItems.on('click', '.favoriteNav', function(event) {
+            // hide only non faves? far is the class of nonfave story
+            $('.far')
+                // closest will find the closest parent that meets the condition?
+                // so will find li name and hide it that also has class of .far
+                .closest('li') 
+                .toggleClass('all')
+                .hide();
+            // changing text to toggle between fav and all
+            // also switch class will only work for jquery UI?
+            // need to remove class and then add a class or would toggle class work better?
+            $(this).text($(this).text() === '| Favorites' ? '| All' : '| Favorites');
+            $(this)
+                .removeClass('favoriteNav')
+                .addClass('allNav');
+        });
+    };
+    // show all function
+    // use .allNav class that was added to the navbar and also use event delegation
+    function showAll() {
+        let navItems = $('#navbarNavAltMarkup');
+
+        navItems.on('click', '.allNav', function(event) {
+            $('.far')
+              .closest('li')
+              .show();
+            $(this).text($(this).text() === '| All' ? '| Favorites' : '| All');
+            $(this)
+                .removeClass('allNav')
+                .addClass('favoriteNav');
         })
     }
+    // i want to say that if the id of switch has a class of allNav then add eventlistener on click to show all
+    // and toggle classes again
+    if ($('#switch.allNav')) {
+        showAll();
+    }
+
     addStory();
     favoriteStar();
     showFavorite();
+    //showAll();
 });
